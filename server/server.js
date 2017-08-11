091f286c3e705e6453c6d9aa507d683768ac5531
Toby J. Henderson <bigtob@gmail.com>
2017-08-11 16:48:26 -0500

    added gitignore file, ADD GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//connect to db with --> james@james-Satellite-P775:~/mongo/bin$ ./mongod --dbpath ~/mongo-data
+var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
-  });  
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
@@ -21,12 +22,31 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {		
-  Todo.find().then((todos) => {		
-    res.send({todos});		
-  }, (e) => {		
-    res.status(400).send(e);		
-  });		
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
@@ -34,4 +54,3 @@ app.listen(3000, () => {
 });
 
 module.exports = {app};
-
